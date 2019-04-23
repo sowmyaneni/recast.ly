4477084fb72237cf72eb3c3b9b2ed78f084fc8a0
sowmyaneni <ssneni@gmail.com>
2019-04-22 21:36:11 -0700

    Implement VideoList specRunner

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,10 +12,11 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos = {exampleVideoData} /> 
       </div>
     </div>
   </div>
+
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,20 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+// var VideoList = (props) => (
+//   <div className="video-list">
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//   </div>
+// );
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map(video =>
+      <VideoListEntry video={video}/>
+    )}
   </div>
 );
 
@@ -13,6 +23,8 @@ var VideoList = () => (
 VideoList.propTypes = {
   videos: React.PropTypes.array.isRequired
 };
+
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
